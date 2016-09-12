758ca61c9b22eeaa388c2fbc12d903a70e2b33d0
Kirill Babkin <babkin.kir@gmail.com>
2016-09-12 13:17:25 -0400

    pomidori app basic 25min work 5min rest sicle

---
+++ src/com/teamtreehouse/pomodoro/model/AttemptKind.java
@@ -0,0 +1,22 @@
+package com.teamtreehouse.pomodoro.model;
+
+public enum AttemptKind {
+    FOCUS(25*60, "Focus Mode"),
+    BREAK(5*60, "Break Time");
+
+    private int mTotalSeconds;
+    private String mDisplayName;
+
+    AttemptKind(int totalSeconds, String displayName) {
+        mDisplayName = displayName;
+        mTotalSeconds = totalSeconds;
+    }
+
+    public int getTotalSeconds() {
+        return mTotalSeconds;
+    }
+
+    public String getDisplayName() {
+        return mDisplayName;
+    }
+}
